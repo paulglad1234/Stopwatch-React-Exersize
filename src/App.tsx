44974251ea121a5c6502027138b099e5ef82da1c
Pavel Gladkikh <pavel.gladkikh@dsr-corporation.com>
2025-03-10 19:20:22 +0300

    [Draft] Stopwatch state (doesn't count time yet)

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,29 @@ import {useState} from 'react'
 import './App.css'
 
 function Stopwatch({onRemove}: { onRemove: () => void }) {
+    const [isRunning, setIsRunning] = useState<boolean | null>(null);
+
+    function getActiveSwControls() {
+        if (isRunning) {
+            return (<>
+                <button onClick={() => setIsRunning(false)}>Pause</button>
+                <button onClick={() => setIsRunning(null)}>Reset</button>
+            </>);
+        }
+        if (isRunning !== null) {
+            return (<>
+                <button onClick={() => setIsRunning(true)}>Resume</button>
+                <button onClick={() => setIsRunning(null)}>Reset</button>
+            </>);
+        }
+        return (<button onClick={() => setIsRunning(true)}>Start</button>);
+    }
+
     return (
         <div className="stopwatch">
             <button onClick={onRemove}>D</button>
             00:00:00
+            {getActiveSwControls()}
         </div>
     );
 }
